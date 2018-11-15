cb35eb71d2baea78e6992cc35645ebed43b55ba0
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-15 14:39:04 +0100

    php exo5

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+$int = (NULL);
+settype($int,"integer");
+echo $int;
+echo " ";
+$int = 2;
+echo $int;
+?>
+</body>
+</html>
